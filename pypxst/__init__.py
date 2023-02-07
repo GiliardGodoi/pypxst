0a64e2be0df665212481717600c0b779d7216aae
Giliard Godoi <giliard.godoi@gmail.com>
2023-02-07 10:45:40 -0300

    black formater

--- pypxst/__init__.py
+++ pypxst/__init__.py
@@ -1,4 +1,4 @@
 from .main import PartitionCrossoverSteinerTree
 from .main import PartitionCrossoverSteinerTree as PXST
 
-__version__ = '0.0.1'
+__version__ = "0.0.0"
--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup, find_packages
 
-setup(
-    name='pypxst',
-    version = '0.0.0',
-    packages=find_packages()
-)
+setup(name="pypxst", version="0.0.0", packages=find_packages())
